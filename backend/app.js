87890bcf1f155d52507b63019d9d24d6a5fe5e5a
Nouaaman <nouaamank1@gmail.com>
2022-04-28 23:19:37 +0200

    stuff model and conroller

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const DB_URL = process.env.DB_URL;
+
+
+const stuffRoutes = require('./routes/stuff');
+
+mongoose.connect(DB_URL)
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas!');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to MongoDB Atlas!');
+        console.error(error);
+    });
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/stuff', stuffRoutes);
+
+module.exports = app;
